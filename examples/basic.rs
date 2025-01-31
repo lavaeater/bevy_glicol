039c9cfd018f64418ff7044a4fd6c47baff5093b
Tommie Nygren <tommie.nygren@gmail.com>
2025-01-31 11:14:19 +0100

    Not working with existing examples, annoying... but we'll figure it out!

--- examples/basic.rs
+++ examples/basic.rs
@@ -12,7 +12,18 @@ fn main() {
 #[derive(Resource)]
 pub struct Vol(f32);
 
+
 fn play_tone(engine: Res<GlicolEngine>, mut vol: ResMut<Vol>, input: Res<ButtonInput<KeyCode>>) {
+    let codes = "out: sin ~pitch >> mul ~envb >> mul 0.9
+
+~envb: ~triggerb >> envperc 0.01 0.4;
+
+~env_pitch: ~triggerb >> envperc 0.01 0.1;
+
+~pitch: ~env_pitch >> mul 50 >> add 60;
+
+~triggerb: speed 4.0 >> seq 60";
+    
     if input.pressed(KeyCode::ArrowUp) {
         vol.0 += 0.01;
         vol.0 = vol.0.min(1.0);
@@ -20,5 +31,6 @@ fn play_tone(engine: Res<GlicolEngine>, mut vol: ResMut<Vol>, input: Res<ButtonI
         vol.0 -= 0.01;
         vol.0 = vol.0.max(0.0);
     }
-    engine.update_with_code(&format!("o: sin {}", vol.0 * 440.0 + 220.0));
+    // engine.update_with_code(&format!("o: sin {}", vol.0 * 440.0 + 220.0));
+    engine.update_with_code(codes);
 }
